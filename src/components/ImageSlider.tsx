9a80a162b01bcda50068239e3de3c99c72c7dde8
Ian <ianez7659@gmail.com>
2025-08-29 21:58:06 -0700

    refactor: Remove unused useState import from ImageSlider component

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
