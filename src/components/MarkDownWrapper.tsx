5808531fede4d4f6e3a00775862f875ef0c0b6ab
Ian <ianez7659@gmail.com>
2025-08-29 22:36:14 -0700

    refactor: Remove redundant Props type definition in MarkdownWrapper component

--- src/components/MarkDownWrapper.tsx
+++ src/components/MarkDownWrapper.tsx
@@ -1,7 +1,3 @@
-type Props = {
-  children: React.ReactNode;
-};
-
 export default function MarkdownWrapper({ children }: { children: React.ReactNode }) {
   return (
     <article className="prose max-w-none">{children}</article>
