d3940606bc739b34db88df733af0fbf15baf9d1b
Ian <ianez7659@gmail.com>
2026-02-22 17:43:04 -0800

    feat: Refactor Callout component to separate background and text colors for better styling

--- src/components/Callout.tsx
+++ src/components/Callout.tsx
@@ -6,11 +6,24 @@ type Props = {
 export default function Callout({ type = "info", children }: Props) {
   const base = "p-2 md:p-4 rounded-md ";
   const colors = {
-    info: "bg-blue-50 border-blue-500 text-red-700",
-    tip: "bg-green-50 border-green-500 text-green-800",
-    warn: "bg-yellow-50 border-yellow-500 text-yellow-800",
-    texting: "text-red-300",
+    info: "bg-blue-50 border-blue-500",
+    tip: "bg-green-50 border-green-500",
+    warn: "bg-yellow-50 border-yellow-500",
+    texting: "",
+  };
+  
+  const textColors = {
+    info: "text-blue-900",
+    tip: "text-green-900",
+    warn: "text-amber-900",
+    texting: "text-red-700",
   };
 
-  return <div className={`${base} ${colors[type]}`}>{children}</div>;
+  return (
+    <div className={`${base} ${colors[type]} my-6`}>
+      <div className={textColors[type]}>
+        {children}
+      </div>
+    </div>
+  );
 }
